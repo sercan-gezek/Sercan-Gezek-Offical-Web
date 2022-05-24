4df8aee32c2c544fa80e949602916e287f440447
sercangezek <sercangezek34@gmail.com>
2022-05-24 23:34:38 +0300

    Add menubar.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
-import anaLogo from "./img/hamburger-menu.svg";
-
 function Header (){
     return(
         <div>
             <div className="menu">
-                <button>
-                    <img className={classes.logo} src={anaLogo} alt="Logo" />
+                <button className="menubar">
+                    <img src="https://sercangezek.com/img/align-left.svg" alt=""/>
                 </button>
+
+                <div className="menu-text">
+                    <p>ANASAYFA</p>
+                    <p>HAKKIMDA</p>
+                    <p>BLOG</p>
+                </div>
             </div>
+
         </div>
     )
 }
